cccb93726ba3e242eba18101f1b8f4946ef21547
Kyron Smith <kds060902@outlook.com>
2022-12-29 21:02:26 +0000

    added layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,17 @@
 import * as React from "react";
-import Navbar from "../components/Navbar";
+import Layout from "../components/Layout";
+
 
 export default function Home() {
   return (
+    <Layout>
     <section>
-      <Navbar/>
       <div>
         <h2>Design</h2>
         <h3>Develop & Deploy</h3>
         <p>UX Designer & web developer</p>
       </div>
     </section>
+    </Layout>
   );
 }
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import Layout from '../../components/Layout'
 
 export default function Projects() {
   return (
+    <Layout>
+
     <div>
         <h2>Portfolio</h2>
         <h3>Project & Websites I have Created</h3>
     </div>
+    </Layout>
   )
 }
