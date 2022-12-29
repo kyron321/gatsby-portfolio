fc73eb738dbeb52e824d910bd5b7f3ca2418588d
Kyron Smith <kds060902@outlook.com>
2022-12-29 20:37:33 +0000

    initial commit

---
+++ src/pages/projects/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Projects() {
+  return (
+    <div>
+        <h2>Portfolio</h2>
+        <h3>Project & Websites I have Created</h3>
+    </div>
+  )
+}
