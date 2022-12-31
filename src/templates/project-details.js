f34f5099b3404435fe459af0ef09678b3569e1bb
Kyron Smith <kds060902@outlook.com>
2022-12-31 23:37:32 +0000

    added detailed about page using graphql query

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -1,19 +1,43 @@
-import React from "react";
-import Img from "gatsby-image";
-import Layout from "../components/Layout";
-import * as styles from "../styles/project-details.module.css";
 
-const ProjectDetails = () => {
-  return (
-    <Layout>
-      <div className={styles.details}>
-        <h2>title</h2>
-        <h3>stack</h3>
-        <div className={styles.featured}>{/* <Img fluid={} /> */}</div>
-        {/* <div className={styles.html} dangerouslySetInnerHTML={} /> */}
-      </div>
-    </Layout>
-  );
-};
+import { graphql } from 'gatsby';
+import React from 'react';
+import Layout from '../components/Layout';
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import * as styles from '../styles/project-details.module.css';
 
-export default ProjectDetails;
+
+export default function ProjectDetails({ data }) {
+    
+    const { html } = data.markdownRemark
+    const { title, stack, featuredImg } = data.markdownRemark.frontmatter
+    
+    return (
+        <Layout>
+            <div className={styles.details}>
+                <h2> { title } </h2>
+                <h3> { stack } </h3>
+                <div className={styles.featured}>
+                    <GatsbyImage image={getImage(featuredImg.childImageSharp.gatsbyImageData)} alt="Projects" />
+                </div>
+                <div className={styles.html} dangerouslySetInnerHTML={{ __html: html }} />
+            </div>
+        </Layout>
+    )
+}
+
+export const query = graphql`
+    query ProjectsPage($slug: String) {
+        markdownRemark(frontmatter: {slug: {eq: $slug}}) {
+            html
+            frontmatter {
+                stack
+                title
+                featuredImg {
+                    childImageSharp {
+                        gatsbyImageData
+                    }
+                }
+            }
+        }
+    }
+`;
